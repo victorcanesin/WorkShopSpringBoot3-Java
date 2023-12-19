b79c2aeff2346989174c8eb5fea7b7d158237804
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-19 15:32:15 -0300

    Service layer and component registration

---
+++ src/main/java/com/curso/cursospringboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.curso.cursospringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.cursospringboot.entities.User;
+import com.curso.cursospringboot.repositories.UserRepository;
+
+@Service // registra a class como componente do spring permitindo que ela seja autoinjetava no resource, no caso é especifico, o genérico é @Component
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	// aqui ja vai a injeção de dependencia do repository e podemos usar ele a vontade	
+	
+	public List<User> findAll (){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
