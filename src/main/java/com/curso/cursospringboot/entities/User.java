a88a31764eb4db9cdec94bdb74e61794d212fb46
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-19 15:47:33 -0300

    part 1 class order

--- src/main/java/com/curso/cursospringboot/entities/User.java
+++ src/main/java/com/curso/cursospringboot/entities/User.java
@@ -1,12 +1,15 @@
 package com.curso.cursospringboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,6 +28,9 @@ public class User implements Serializable{
 	private String phone;
 	
 	private String password;
+	
+	@OneToMany(mappedBy="client") // um para muitos - indica o nome do atributo da classe user mapeia o cliente no pedido
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -78,6 +84,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public List<Order> getOrders (){
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
