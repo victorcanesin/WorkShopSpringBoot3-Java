bccb5707c46d73cdd7d70a948175395c51ca5abc
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-22 11:02:14 -0300

    associação n para n produto e item

--- src/main/java/com/curso/cursospringboot/entities/Product.java
+++ src/main/java/com/curso/cursospringboot/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,41 +14,44 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_product")
-public class Product implements Serializable {	
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
-	
+
 	private String descricao;
-	
+
 	private Double price;
-	
+
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_categories", 
-	joinColumns=@JoinColumn(name="fk_produto_id"),
-	inverseJoinColumns=@JoinColumn(name="fk_category_id"))
+	@JoinTable(name = "tb_product_categories", joinColumns = @JoinColumn(name = "fk_produto_id"), inverseJoinColumns = @JoinColumn(name = "fk_category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
+	@OneToMany(mappedBy = "id.product") // product tem um id do tipo OrderItemPK e LA POSSUI o produto na chave
+	private Set<OrderItem> itens = new HashSet<>(); // mapeando para que seja possivel chegar aos pedidos de cada
+													// produto
+
 	public Product() {
-		
+
 	}
 
-	public Product(Long id, String name, String descricao, Double price, String imgUrl) {		
+	public Product(Long id, String name, String descricao, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
 		this.descricao = descricao;
 		this.price = price;
-		this.imgUrl = imgUrl;	
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -89,8 +94,21 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-	public  Set<Category> getCategory() {
+	public Set<Category> getCategory() {
 		return categories;
+	}
+
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		
+		//percorde os itens que estao vinculados a esse produto
+		// e captura os pedidos
+		Set<Order> set = new HashSet<>();
+
+		for (OrderItem item : itens) {
+			set.add(item.getOrder());
+		}
+		return set;
 	}
 
 	@Override
