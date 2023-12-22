2284f559b76814f67de8396f099ddb4ba3d2c3cb
Victor Canesin <victorcanesin.rx@gmail.com>
2023-12-22 15:07:51 -0300

    update

--- src/main/java/com/curso/cursospringboot/services/UserService.java
+++ src/main/java/com/curso/cursospringboot/services/UserService.java
@@ -13,50 +13,57 @@ import com.curso.cursospringboot.repositories.UserRepository;
 import com.curso.cursospringboot.services.exceptions.DatabaseException;
 import com.curso.cursospringboot.services.exceptions.ResourceNotFoundException;
 
-@Service // registra a class como componente do spring permitindo que ela seja autoinjetava no resource, no caso é especifico, o genérico é @Component
+import jakarta.persistence.EntityNotFoundException;
+
+@Service // registra a class como componente do spring permitindo que ela seja
+			// autoinjetava no resource, no caso é especifico, o genérico é @Component
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	// aqui ja vai a injeção de dependencia do repository e podemos usar ele a vontade	
-	
-	public List<User> findAll (){
+	// aqui ja vai a injeção de dependencia do repository e podemos usar ele a
+	// vontade
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		//orElseThrow  > vai tentar dar o get, se nao tiver usuario estoura a exception
+		// orElseThrow > vai tentar dar o get, se nao tiver usuario estoura a exception
 	}
-	
+
 	public User insert(User user) {
 		return repository.save(user);
 	}
-	
+
 	public void delete(Long id) {
-		
+
 		try {
 			repository.deleteById(id);
-		}
-		catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
-	
+
 	public User update(Long id, User user) {
-		// get reference by id apenas monitora o registro, findbyId iria direto no banco buscar ele
-		User entity = repository.getReferenceById(id);
-		updateDada(entity, user);
-		return repository.save(entity);
+		try {
+			// get reference by id apenas monitora o registro, findbyId iria direto no banco
+			// buscar ele
+			User entity = repository.getReferenceById(id);
+			updateDada(entity, user);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateDada(User entity, User user) {
 		entity.setName(user.getName());
 		entity.setPhone(user.getPhone());
-		entity.setEmail(user.getEmail());		
+		entity.setEmail(user.getEmail());
 	}
 }
